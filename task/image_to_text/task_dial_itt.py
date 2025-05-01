277b032df06895c9ced37c51309e586c0526a4f5
goal <success.goal31121@gmail.com>
2025-05-01 10:00:00 +0900

    created project

---
+++ task/image_to_text/task_dial_itt.py
@@ -0,0 +1,44 @@
+import asyncio
+from io import BytesIO
+from pathlib import Path
+
+from task._models.custom_content import Attachment, CustomContent
+from task._utils.constants import API_KEY, DIAL_URL, DIAL_CHAT_COMPLETIONS_ENDPOINT
+from task._utils.bucket_client import DialBucketClient
+from task._utils.model_client import DialModelClient
+from task._models.message import Message
+from task._models.role import Role
+
+
+async def _put_image() -> Attachment:
+    file_name = 'dialx-banner.png'
+    image_path = Path(__file__).parent.parent.parent / file_name
+    mime_type_png = 'image/png'
+    # TODO:
+    #  1. Create DialBucketClient
+    #  2. Open image file
+    #  3. Use BytesIO to load bytes of image
+    #  4. Upload file with client
+    #  5. Return Attachment object with title (file name), url and type (mime type)
+    raise NotImplementedError
+
+
+def start() -> None:
+    # TODO:
+    #  1. Create DialModelClient
+    #  2. Upload image (use `_put_image` method )
+    #  3. Print attachment to see result
+    #  4. Call chat completion via client with list containing one Message:
+    #    - role: Role.USER
+    #    - content: "What do you see on this picture?"
+    #    - custom_content: CustomContent(attachments=[attachment])
+    #  ---------------------------------------------------------------------------------------------------------------
+    #  Note: This approach uploads the image to DIAL bucket and references it via attachment. The key benefit of this
+    #        approach that we can use Models from different vendors (OpenAI, Google, Anthropic). The DIAL Core
+    #        adapts this attachment to Message content in appropriate format for Model.
+    #  TRY THIS APPROACH WITH DIFFERENT MODELS!
+    #  Optional: Try upload 2+ pictures for analysis
+    raise NotImplementedError
+
+
+start()
---
+++ task/text_to_image/task_tti.py
@@ -0,0 +1,57 @@
+import asyncio
+from datetime import datetime
+
+from task._models.custom_content import Attachment
+from task._utils.constants import API_KEY, DIAL_URL, DIAL_CHAT_COMPLETIONS_ENDPOINT
+from task._utils.bucket_client import DialBucketClient
+from task._utils.model_client import DialModelClient
+from task._models.message import Message
+from task._models.role import Role
+
+class Size:
+    """
+    The size of the generated image.
+    """
+    square: str = '1024x1024'
+    height_rectangle: str = '1024x1792'
+    width_rectangle: str = '1792x1024'
+
+
+class Style:
+    """
+    The style of the generated image. Must be one of vivid or natural.
+     - Vivid causes the model to lean towards generating hyper-real and dramatic images.
+     - Natural causes the model to produce more natural, less hyper-real looking images.
+    """
+    natural: str = "natural"
+    vivid: str = "vivid"
+
+
+class Quality:
+    """
+    The quality of the image that will be generated.
+     - ‘hd’ creates images with finer details and greater consistency across the image.
+    """
+    standard: str = "standard"
+    hd: str = "hd"
+
+async def _save_images(attachments: list[Attachment]):
+    # TODO:
+    #  1. Create DIAL bucket client
+    #  2. Iterate through Images from attachments, download them and then save here
+    #  3. Print confirmation that image has been saved locally
+    raise NotImplementedError
+
+
+def start() -> None:
+    # TODO:
+    #  1. Create DialModelClient
+    #  2. Generate image for "Sunny day on Bali"
+    #  3. Get attachments from response and save generated message (use method `_save_images`)
+    #  4. Try to configure the picture for output via `custom_fields` parameter.
+    #    - Documentation: See `custom_fields`. https://dialx.ai/dial_api#operation/sendChatCompletionRequest
+    #  5. Test it with the 'imagegeneration@005' (Google image generation model)
+    raise NotImplementedError
+
+
+start()
